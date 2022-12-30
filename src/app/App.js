be829530f8d1827afe6310e8968fbf909a3b0c07
Pranav Chauhan <chauhan.pa90@gmail.com>
2022-12-30 23:07:53 +0530

    portfolio website
    this is my portfolio website made with reactjs

---
+++ src/app/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  useLocation,
+  withRouter,
+} from "react-router-dom";
+import AppRoutes from "./routes";
+import Headermain from "../header";
+
+import "./App.css";
+
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
+}
+const ScrollToTop = withRouter(_ScrollToTop);
+
+export default function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="cursor__dot">
+        
+      </div>
+      <ScrollToTop>
+        <Headermain />
+        <AppRoutes />
+      </ScrollToTop>
+    </Router>
+  );
+}
---
+++ src/content_option.js
@@ -0,0 +1,206 @@
+const logotext = "PRANAV CHAUHAN";
+const meta = {
+    title: "Pranav Chauhan",
+    description: "I’m Pranav Chauhan, data scientist, Full stack devloper and a profissional photographer, currently working nowhere",
+};
+
+const introdata = {
+    title: "I’m Pranav Chauhan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I capture memories",
+    },
+    description: "I'm Pranav Chauhan, software engineer, a professional photgrapher and a stock trader.",
+
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am Pranav Chauhan. i want to enrich my knowledge through learning and to utilize my technical skills & behavioural skills for the development of the organization and to enrich my knowledge as well.",
+};
+const worktimeline = [{
+        jobtitle: "Graduation (B.Tech)",
+        where: "Kurukshetra University",
+        date: "2023",
+    },
+    {
+        jobtitle: "12th",
+        where: "Panipat",
+        date: "2019",
+    },
+    {
+        jobtitle: "10th",
+        where: "panipat",
+        date: "2017",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const projects = [{
+        title: "Amazon Clone",
+        description: "Built an front end of an e-commerce website using reactjs, and css.",
+    },
+    {
+        title: "E-mail Spam classifer",
+        description: "Build an app in jupyter notebook, which can specify the emails into two main categories spam and ham using machine learning",
+    },
+    {
+        title: "Portfolio",
+        description: "Built an personal portfolio website.",
+    },
+    {
+        title: "Snake Game",
+        description: "Built an snake game using tkiner library in python.",
+    },
+   
+];
+
+const certificates = [{
+    title: "●	Python Certificate from Coursera",    
+},
+{
+    title: "●	Web development Certificate from Uptoskills.",
+    
+},
+{
+    title: "●	HTML&CSS Certificate from Udemy.",
+    
+},
+{
+    title: "●	Advanced Excel  from elearnmarkets.",
+    
+},
+{
+    title: "●	Technical  Analysis course from finnovationz.com",
+    
+},
+
+
+
+
+];
+
+
+
+
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "pranavchauhann@gmail.com",
+    YOUR_FONE: "8685998979",
+    description: "I'm Pranav Chauhan, software engineer, a professional photgrapher and a stock trader. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    projects,
+    certificates,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    
+};
